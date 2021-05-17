35e68a4e881eba419419951bc90d7266014782ac
Ilnar Gomelyanov <hibushland@gmail.com>
2021-05-17 19:40:04 +0300

    add create_chart

---
+++ work_with_data/create_chart.py
@@ -0,0 +1,19 @@
+from numpy import median
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def create_chart(csv_file_path: str):
+    data = pd.read_csv(csv_file_path)
+    data.set_index("size", inplace=True) 
+
+    plt.plot(data["min"], color="#008000", label="min")
+    plt.plot(data["max"], color="#ff0000", label="max")
+    plt.plot(data["avg"], color="#ffa500", label="avg")
+    plt.plot(data["median"], color="#8b00ff", label="median")
+    
+    plt.legend()
+    plt.savefig("mygraph.png")
+
+PATH = "/home/bushmester/study/study_programming/cpp/dijkstra-algorithm/work_with_data/load_testing_mesurenets/1621259609.543206.csv"
+create_chart(PATH)
