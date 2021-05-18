abec72ef560acfafe8ff01db457497499a530d28
Ilnar Gomelyanov <hibushland@gmail.com>
2021-05-18 15:13:58 +0300

    fix create chart

--- work_with_data/create_chart.py
+++ work_with_data/create_chart.py
@@ -1,11 +1,10 @@
-from numpy import median
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
 def create_chart(csv_file_path: str):
     data = pd.read_csv(csv_file_path)
-    data.set_index("size", inplace=True) 
+    data.set_index("size", inplace=True)
     data = data.sort_values("size")
 
     plt.plot(data["min"], color="#008000", label="min")
@@ -15,6 +14,7 @@ def create_chart(csv_file_path: str):
 
     plt.legend()
     plt.savefig("mygraph.png")
+
 
 PATH = "/home/bushmester/study/study_programming/cpp/dijkstra-algorithm/work_with_data/load_testing_mesurenets/1621278390.2586138.csv"
 create_chart(PATH)
